69b1e43a3cdf9502cfa73c6d60057dd90d9b7c58
endercrest <tcvs@endercrest.com>
2017-09-16 13:04:18 -0400

    AndroidRenderer Update

--- frontend/src/components/AndroidRenderer.js
+++ frontend/src/components/AndroidRenderer.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField'
+import MouseTracker from './MouseTracker'
+import List from "material-ui/List"
+import ListItem from "material-ui/List"
+import Divider from 'material-ui/Divider';
 import '../App.css';
 
 class AndroidRenderer extends Component{
@@ -8,13 +14,71 @@ class AndroidRenderer extends Component{
             return null
 
         var children = [];
-        if(element.children != null && element.children.length > 0){
-            element.children.forEach((child) => {
-                children.push(this.renderElement(child, offset));
-                console.log(child);
-            });
+        
+        var style = {
+            height: Math.ceil(element.height/4) + "px",
+            width: Math.ceil(element.width/4) + "px",
+            position: "absolute",
+            top: (element.y - offset)/4 + "px",
+            left: element.x/4 + "px",
+            fontSize: element.textSize/4,
+            //color: element.textColor,
+            //backgroundColor: element.backgroundColor
         }
 
+        switch(element.type){
+            case "group":
+                if(element.children != null && element.children.length > 0){
+                    element.children.forEach((child) => {
+                        children.push(this.renderElement(child, offset));
+                        console.log(child);
+                    });
+                }
+
+                return (<div style={style}>{children.map((child, index) => { return (<div id={index}>{child}</div>);})}</div>)
+            case "list":
+                if(element.children != null && element.children.length > 0){
+                    element.children.forEach((child) => {
+                        children.push(this.renderListItem(child, offset));
+                        console.log(child);
+                    });
+                }
+
+                return (<MouseTracker style={style}><List>{children.map((child, index) => { return (<div id={index}>{child}</div>);})}</List></MouseTracker>)
+            case "button":
+                var placementStyle = {
+                    position: "absolute",
+                    top: (element.y - offset)/4 + "px",
+                    left: element.x/4 + "px",
+                    fontSize: element.textSize/4,
+                }
+
+                var buttonStyle = { }
+
+
+                if(element.backgroundColor)
+                    placementStyle.background = element.backgroundColor;
+                if(element.textColor)
+                    placementStyle.color = element.textColor;
+
+                return <RaisedButton labelColor={element.textColor} backgroundColor={element.backgroundColor} style={placementStyle} buttonStyle={buttonStyle}>{element.text}</RaisedButton>
+            case "input":
+                var inputStyle = {}
+
+                if(element.backgroundColor)
+                    inputStyle.background = element.backgroundColor;
+                if(element.textColor)
+                    inputStyle.color = element.textColor;
+
+                return <TextField id={element.id} value={element.text} placeholder={element.hint} style={style} inputStyle={inputStyle}></TextField>
+            case "text":
+                return <text style={style}>{element.text}</text>
+            default:
+                return <div style={style}>???</div>
+        }       
+    }
+
+    renderListItem(element, offset){
         var style = {
             height: Math.ceil(element.height/4) + "px",
             width: Math.ceil(element.width/4) + "px",
@@ -24,18 +88,7 @@ class AndroidRenderer extends Component{
             fontSize: element.textSize/4,
         }
 
-        switch(element.type){
-            case "group":
-                return (<div style={style}>{children.map((child, index) => { return (<div id={index}>{child}</div>);})}</div>)
-            case "button":
-                return <button style={style}>Button</button>
-            case "input":
-                return <input placeholder={element.hint} style={style}/>
-            case "text":
-                return <p style={style}>{element.text}</p>
-            default:
-                return <div style={style}>???</div>
-        }       
+        return (<div style={style}><ListItem>{element.text}</ListItem><Divider/></div>)
     }
 
     render(){
