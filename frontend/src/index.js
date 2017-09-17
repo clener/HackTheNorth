35ba817cfa04daa835b39fa7b3d62318130d6b6a
Claudio Lener <claudiolener98@gmail.com>
2017-09-16 21:31:22 -0400

    implemented routing at the index level

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import IssueList from './Components/IssueList';
+import Android from './Components/AndroidRenderer'
 import registerServiceWorker from './registerServiceWorker';
+import { IndexRoute, Router, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={MainScreen} />
+      <Route path="/android" component={Android} />
+    </div>
+  </BrowserRouter>, 
+  document.getElementById('root'));
 registerServiceWorker();
