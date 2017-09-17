375ce3768e3c36d2dcbe8aa0a67c02e071f6916a
Neel Mehta <neelmehta247@gmail.com>
2017-09-16 21:15:57 -0400

    Made the Android app funky

---
+++ android/app/src/main/java/me/neelmehta/hackthenorth/SecondActivity.java
@@ -0,0 +1,55 @@
+package me.neelmehta.hackthenorth;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import me.neelmehta.hackthenorth.pluggable.Pluggable;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        findViewById(R.id.takeBack).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.makeRed).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.takeBack).setBackground(new ColorDrawable(Color.RED));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Pluggable.plug(this, findViewById(R.id.secondRoot));
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        boolean val = super.dispatchTouchEvent(ev);
+        Pluggable.reRender(findViewById(R.id.secondRoot));
+        return val;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Pluggable.unplug(this);
+    }
+}
